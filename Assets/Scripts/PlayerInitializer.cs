78d31c1781c1c071988f425e27cc7c95c864c137
Jett McWhorter <158016844+Jettmcw@users.noreply.github.com>
2026-02-17 17:02:15 -0600

    Players Now Start In Distinct Predefined Locations

--- Assets/Scripts/PlayerInitializer.cs
+++ Assets/Scripts/PlayerInitializer.cs
@@ -5,7 +5,7 @@ public class PlayerInitializer : MonoBehaviour
 	public void Awake()
 	{
 		PlayerControllerManager pcm = PlayerControllerManager.Instance;
-		transform.SetParent(pcm.Canvas);
+		transform.SetParent(pcm.Canvas.GetChild(pcm.Players.Count - 1));
 		transform.localScale = Vector3.one * 100;
 		transform.localPosition = Vector3.zero;
 	}
