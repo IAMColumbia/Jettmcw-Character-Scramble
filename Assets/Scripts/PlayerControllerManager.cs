a640d251736bcb345043345c3c2979a9f80df6bc
Jett McWhorter <158016844+Jettmcw@users.noreply.github.com>
2026-03-05 01:28:23 -0600

    Finagling with input actions

--- Assets/Scripts/PlayerControllerManager.cs
+++ Assets/Scripts/PlayerControllerManager.cs
@@ -17,6 +17,8 @@ public class PlayerControllerManager : MonoBehaviour
     {
         Players.Add(player);
 
+		Debug.Log(player.currentControlScheme);
+
 		player.transform.SetParent(Canvas.GetChild(Players.Count - 1));
 		player.transform.localScale = Vector3.one * 250;
 		player.transform.localPosition = Vector3.zero;
