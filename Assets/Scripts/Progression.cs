df96d194d1b0fc0b02630e331531a67da477a4c6
Jett McWhorter <158016844+Jettmcw@users.noreply.github.com>
2026-03-06 08:10:41 -0600

    Cycling now called separately from animation

--- Assets/Scripts/Progression.cs
+++ Assets/Scripts/Progression.cs
@@ -1,21 +1,24 @@
-using System;
-using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Progression : MonoBehaviour
 {
 	[SerializeField] private PlayerInput _playerInput;
+
+	[SerializeField] private List<Color> _selectedColors;
 	[SerializeField] private PlayerCycling[] _rows;
 	[SerializeField] private int _rowIndex = 0;
 
 	private void Awake()
 	{
 		_rows[0].Register();
+		_selectedColors.Add(_rows[0].Current);
 	}
 
 	public void GoBack()
 	{
+		_selectedColors.RemoveAt(_rowIndex);
 		if (_rowIndex == 0)
 		{
 			Debug.Log("Back!");
@@ -39,6 +42,7 @@ public class Progression : MonoBehaviour
 		Transform area = PlayerAreas.GetArea(_playerInput);
 		area.GetChild(0).gameObject.SetActive(false);
 		_rows[0].Register();
+		_selectedColors.Add(_rows[0].Current);
 		_playerInput.SwitchCurrentActionMap("Player Controls");
 	}
 
@@ -50,5 +54,12 @@ public class Progression : MonoBehaviour
 		}
 
 		Debug.Log("No!");
+	}
+
+	public void Cycle(bool right)
+	{
+		PlayerCycling row = _rows[_rowIndex];
+		row.Cycle(right);
+		_selectedColors[_rowIndex] = row.Current;
 	}
 }
