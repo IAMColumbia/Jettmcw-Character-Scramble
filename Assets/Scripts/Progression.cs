8ff2a49b50a04f854731fb03c9fd41cf0af7b1af
Jett McWhorter <158016844+Jettmcw@users.noreply.github.com>
2026-03-06 07:49:05 -0600

    Player can now press any button to rejoin game

--- Assets/Scripts/Progression.cs
+++ Assets/Scripts/Progression.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -16,11 +18,35 @@ public class Progression : MonoBehaviour
 	{
 		if (_rowIndex == 0)
 		{
-			_rowIndex--;
+			Debug.Log("Back!");
+			_rowIndex = -1;
 			Transform area = PlayerAreas.GetArea(_playerInput);
 			area.GetChild(0).gameObject.SetActive(true);
 			_rows[0].Release();
+			StartCoroutine(AllowRejoin());
 			return;
 		}
+	}
+
+	private IEnumerator AllowRejoin()
+	{
+		yield return null;
+		_playerInput.actionEvents[3].AddListener(TryRejoin);
+	}
+
+	public void TryRejoin(InputAction.CallbackContext rejoin)
+	{
+		Debug.Log("Rejoin!");
+		_playerInput.actionEvents[3].RemoveListener(TryRejoin);
+		_rowIndex = 0;
+		Transform area = PlayerAreas.GetArea(_playerInput);
+		area.GetChild(0).gameObject.SetActive(false);
+		_rows[0].Register();
+		return;
+	}
+
+	public void GoForward()
+	{
+		Debug.Log("No!");
 	}
 }
