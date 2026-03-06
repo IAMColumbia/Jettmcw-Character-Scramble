76f0ebbe4f12440ec157267edca5b18a409dd7e0
Jett McWhorter <158016844+Jettmcw@users.noreply.github.com>
2026-03-06 07:12:27 -0600

    Entering game now removes "Press Any Button" text

--- Assets/Scripts/PlayerSetup.cs
+++ Assets/Scripts/PlayerSetup.cs
@@ -10,5 +10,6 @@ public class PlayerSetup : MonoBehaviour
 		Transform area = PlayerAreas.GetArea(_player);
 		transform.SetParent(area, true);
 		transform.localPosition = Vector3.zero;
+		area.GetChild(0).gameObject.SetActive(false); // Remove "Press any button" text
 	}
 }
