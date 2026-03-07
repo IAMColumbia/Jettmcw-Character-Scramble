1f2fc4b78efd51d3091c7031d5ff734f40240733
Jett McWhorter <158016844+Jettmcw@users.noreply.github.com>
2026-03-07 14:04:26 -0600

    Added Control Filterer Behavior
    This behavior directly listens to the player input's events, and determines if it's correct or incorrect, invoking a "correct" or "incorrect" behavior accordingly.

--- Assets/Scripts/ControlFilterer.cs
+++ Assets/Scripts/ControlFilterer.cs
@@ -1,16 +1,70 @@
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
 public class ControlFilterer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+	[Header("Cycle Action")]
+	[SerializeField] private string[] _cycleActionNames;
+	public int CycleActionIndex;
+	public UnityEvent<InputAction.CallbackContext> CorrectCycleInput, IncorrectCycleInput;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	[Header("Select Action")]
+	[SerializeField] private string[] _selectActionNames;
+	public int SelectActionIndex;
+	public UnityEvent<InputAction.CallbackContext> CorrectSelectInput, IncorrectSelectInput;
+
+	[Header("Back Action")]
+	[SerializeField] private string[] _backActionNames;
+	public int BackActionIndex;
+	public UnityEvent<InputAction.CallbackContext> CorrectBackInput, IncorrectBackInput;
+
+	private void OnEnable()
+	{
+		ControlRandomizer.Instance.ControlSchemeChanged.AddListener(SetFilters);
+		SetFilters();
+	}
+
+	private void OnDisable()
+	{
+		ControlRandomizer.Instance.ControlSchemeChanged.RemoveListener(SetFilters);
+	}
+
+	private void SetFilters()
+	{
+		ControlRandomizer cr = ControlRandomizer.Instance;
+		CycleActionIndex = cr.FilterIdxs[0];
+		SelectActionIndex = cr.FilterIdxs[1];
+		BackActionIndex = cr.FilterIdxs[2];
+	}
+
+	public void FilterCycleAction(InputAction.CallbackContext context)
+	{
+		FilterAction(context, _cycleActionNames, CycleActionIndex, CorrectCycleInput, IncorrectCycleInput);
+	}
+
+	public void FilterSelectAction(InputAction.CallbackContext context)
+	{
+		FilterAction(context, _selectActionNames, SelectActionIndex, CorrectSelectInput, IncorrectSelectInput);
+	}
+
+	public void FilterBackAction(InputAction.CallbackContext context)
+	{
+		FilterAction(context, _backActionNames, BackActionIndex, CorrectBackInput, IncorrectBackInput);
+	}
+
+	private void FilterAction(
+		InputAction.CallbackContext context,
+		string[] actions,
+		int index,
+		UnityEvent<InputAction.CallbackContext> correctEvent,
+		UnityEvent<InputAction.CallbackContext> incorrectEvent)
+	{
+		string action = context.action.name;
+		string correct = actions[index];
+		bool wasCorrect = action == correct;
+		Debug.Log(wasCorrect);
+		var eventToInvoke = wasCorrect ? correctEvent : incorrectEvent;
+		eventToInvoke.Invoke(context);
+	}
 }
