8de5e68d3a8905fd79f6d268cb87714d358d1d4a
Jett McWhorter <158016844+Jettmcw@users.noreply.github.com>
2026-03-11 16:53:48 -0500

    Smoothed movement

--- Assets/Scripts/ActualMovement.cs
+++ Assets/Scripts/ActualMovement.cs
@@ -1,19 +1,26 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Windows;
 
 public class ActualMovement : MonoBehaviour
 {
 	[SerializeField] private Rigidbody2D rb;
+	Vector2 moveIn = Vector2.zero;
 
 	public void Move(InputAction.CallbackContext context)
 	{
-		Vector2 input = context.ReadValue<Vector2>();
-		rb.linearVelocityX = input.x * 3;
-		float stretch = 1f + (input.y * 0.5f);
-		transform.localScale = new Vector3(1f / stretch, stretch);
+		moveIn = context.ReadValue<Vector2>();
 	}
 
-	public void Jump(InputAction.CallbackContext context)
+	public void FixedUpdate()
+	{
+		rb.linearVelocityX = moveIn.x * 3;
+		float stretch = 1f + (moveIn.y * 0.5f);
+		Vector3 newScale = Vector3.Lerp(transform.localScale, new Vector3(1f / stretch, stretch), 0.2f);
+		transform.localScale = newScale;
+	}
+
+	public void Jump(InputAction.CallbackContext _)
 	{
 		rb.AddForceY(200f);
 		float randomAngle = Random.Range(-5f, 5f);
